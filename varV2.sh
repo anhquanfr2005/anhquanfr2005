4bbd35cf23f14ec351947c2d341effa1c18212eb
anhquanfr2005 <152523926+anhquanfr2005@users.noreply.github.com>
2023-11-30 18:09:32 +0100

    Add files via upload

---
+++ varV2.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+#cp -ir /root/GNS3/configs /home/stock
+
+mkdir /home/stock 2> t.tmp
+
+z=$(($RANDOM % 1000))
+m=$(ls /root/GNS3/configs | tail -1)
+cp -r /root/GNS3/configs/$m /home/stock/$z
+
+if [ $(($RANDOM % 3)) == 1 ];
+    then
+    echo "b"
+    z=$(($RANDOM % 1000))
+    cp -r /root/GNS3/configs/$m /home/stock/$z
+else
+    nom=$(ls /home/stock | tail -1)
+    rm /home/stock/$nom 2> t.tmp
+    nom=$(ls /home/stock | tail -1)
+    rm /home/stock/$nom 2> t.tmp
+    nom=$(ls /home/stock | tail -1)
+    rm /home/stock/$nom 2> t.tmp
+    
+    z=$(($RANDOM % 1000))
+    cp -r /root/GNS3/configs/$m /home/stock/$z
+fi
+exit 0
+
