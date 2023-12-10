17b57a9965ce2d1fbb956199401912c189ac38f6
anhquanfr2005 <152523926+anhquanfr2005@users.noreply.github.com>
2023-12-10 21:19:12 +0100

    Add files via upload

---
+++ PYTHON_PROJET_SCP/stat_calcul_donnee.py
@@ -0,0 +1,26 @@
+import csv
+import matplotlib.pyplot as plt
+
+
+def listeF(fichier : str) -> list[list[str]] :
+    """Fonction qui lit le fichier en paramètre et le retourne sous forme de liste
+
+    Args:
+        fichier (str): chemin du fichier à parcourir
+
+    Returns:
+        list[list[str]]: liste contenant les listes de [ taille, timestamp ]
+    """
+    listeR : list[list[str]] = []
+    
+    with open(fichier, newline='') as csvfile:
+        datareader = csv.reader(csvfile, delimiter= ',', quotechar='|')
+        for ligne in datareader :
+            print(ligne)# on passe en commentaire pour n'avoir que la liste que l'on souhaite
+            listeR.append(ligne)
+    
+    return listeR
+
+
+
+
